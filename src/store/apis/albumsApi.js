7cc4efde21d35c5052bc9473c494ba72f11a285e
Pogtato <erikdeltaco@gmail.com>
2023-02-19 18:46:28 +0100

    album api

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -3,4 +3,39 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const albumsApi = createApi({
   reducerPath: "albumsApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001" }),
+  endpoints(builder) {
+    return {
+      getAlbum: builder.query({
+        query: (userId) => {
+          return {
+            url: `/albums/${userId}`,
+            method: "GET",
+          };
+        },
+      }),
+      deleteAlbum: builder.mutation({
+        query: (albumId) => {
+          return {
+            url: `/albums/${albumId}/delete`,
+            method: "DELETE",
+          };
+        },
+      }),
+      addAlbum: builder.mutation({
+        query: (values) => {
+          return {
+            url: `/albums/post`,
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              title: values.title,
+              userId: values._id,
+            }),
+          };
+        },
+      }),
+    };
+  },
 });
