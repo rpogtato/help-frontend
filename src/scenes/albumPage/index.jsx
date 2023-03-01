4e564153387c5387e3b1053fc5c6a900171030fe
Pogtato <erikdeltaco@gmail.com>
2023-03-01 21:29:17 +0100

    filler commit

--- src/scenes/albumPage/index.jsx
+++ src/scenes/albumPage/index.jsx
@@ -1,7 +1,33 @@
 import { useGetAlbumQuery } from "../../store";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function AlbumPage() {
+  const navigate = useNavigate();
   const { userId } = useParams();
   const { data, error, isLoading } = useGetAlbumQuery(userId);
+
+  let content;
+  if (isLoading) {
+    content = <div>Loading...</div>;
+  } else if (error) {
+    content = <div>Error loading data...</div>;
+  } else {
+    content = data.map((album) => {
+      return (
+        <div key={album._id}>
+          <div>{album.title}</div>
+        </div>
+      );
+    });
+  }
+  return (
+    <>
+      <div>
+        <button onClick={() => navigate(`/profile/${userId}`)}>return</button>
+      </div>
+      <hr />
+      <div>{content}</div>
+    </>
+  );
 }
